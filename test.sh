4e09c4be44b5aa7836f20755b0e5e00d19ed2019
Kamil Trzcinski <ayufan@ayufan.eu>
2016-04-25 01:02:12 +0200

    Fix hooks

--- test.sh
+++ test.sh
@@ -2,4 +2,4 @@
 
 set -e
 docker build -t composer . &>/dev/null
-docker run -it -v /var/run/docker.sock:/var/run/docker.sock --rm composer "$@"
+docker run -a stdin -t -v /var/run/docker.sock:/var/run/docker.sock --rm composer "$@"
