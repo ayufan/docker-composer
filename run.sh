884b6b7c2f4ab36bd2e6b5e9d35ebd6cf6ba6b67
Kamil Trzcinski <ayufan@ayufan.eu>
2016-04-25 00:03:12 +0200

    Added run script

--- run.sh
+++ run.sh
@@ -1,5 +1,31 @@
-#!/bin/bash
+#!/bin/sh
+#
+# Run docker-composer in a container
 
 set -e
-docker build -t composer . &>/dev/null
-docker run -it -v /var/run/docker.sock:/var/run/docker.sock --rm composer "$@"
+
+VERSION="1.7.0"
+IMAGE="ayufan/composer:latest"
+DOCKER_RUN_OPTIONS=""
+DOCKER_ADDR=""
+VOLUMES="-v /srv/apps:/srv/apps"
+
+# Setup options for connecting to docker host
+if [ -z "$DOCKER_HOST" ]; then
+    DOCKER_HOST="/var/run/docker.sock"
+fi
+if [ -S "$DOCKER_HOST" ]; then
+    DOCKER_ADDR="-v $DOCKER_HOST:$DOCKER_HOST -e DOCKER_HOST"
+else
+    DOCKER_ADDR="-e DOCKER_HOST -e DOCKER_TLS_VERIFY -e DOCKER_CERT_PATH"
+fi
+
+# Only allocate tty if we detect one
+if [ -t 1 ]; then
+    DOCKER_RUN_OPTIONS="-t"
+fi
+if [ -t 0 ]; then
+    DOCKER_RUN_OPTIONS="$DOCKER_RUN_OPTIONS -i"
+fi
+
+exec docker run --rm $DOCKER_RUN_OPTIONS $DOCKER_ADDR $VOLUMES $IMAGE "$@"
